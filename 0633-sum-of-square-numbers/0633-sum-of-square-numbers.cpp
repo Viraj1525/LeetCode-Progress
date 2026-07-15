6c726ac5422cccc6aa5dc95fad3bf57a3877ad95
Viraj <2300521520073@ietlucknow.ac.in>
2026-07-15 23:55:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (55.17%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long left = 0;
+        long right = sqrt(c);
+
+        while(left <= right){
+            long long sum = left*left + right*right;
+
+            if(sum == c){
+                return true;
+            } else if(sum > c){
+                right--;
+            } else {
+                left++;
+            }
+
+        }
+        return false;
+        
+    }
+};
