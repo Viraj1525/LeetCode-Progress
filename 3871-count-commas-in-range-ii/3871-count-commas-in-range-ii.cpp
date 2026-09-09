fc76891b6e696a99f36da67e21d0ce0c6b36c6e1
Viraj <virajagrawal.1525@gmail.com>
2026-09-09 16:17:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,21 +3,11 @@ public:
     long long countCommas(long long n) {
         long long ans = 0;
 
-        if (n >= 1000)
-            ans += n - 999;
-
-        if (n >= 1000000)
-            ans += n - 999999;
-
-        if (n >= 1000000000)
-            ans += n - 999999999;
-
-        if (n >= 1000000000000)
-            ans += n - 999999999999;
-
-        if (n >= 1000000000000000)
-            ans += n - 999999999999999;
+        for(long long x = 1000; x <=n; x*=1000){
+            ans += n - x + 1;
+        }
 
         return ans;
+        
     }
 };
